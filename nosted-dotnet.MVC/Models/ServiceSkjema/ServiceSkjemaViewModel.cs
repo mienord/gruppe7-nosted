05831022e1fb4be094b2adceb4faf1fb22ff61b9
martinstereo <martinsteiro02@gmail.com>
2023-09-21 13:05:42 +0200

    test

---
+++ nosted-dotnet.MVC/Models/ServiceSkjema/ServiceSkjemaViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace nosted_dotnet.MVC.Models.ServiceSkjema
+{
+    public class ServiceSkjemaViewModel
+    {
+        public string Mechanic { get; set; }
+        public bool IsAdministrator { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal ConsumedHours { get; set; }
+        public string ImageUrl { get; set; }
+        public string MechanicComment { get; set; }
+        public string CustomerComment { get; set; }
+
+        public string FutureMaintenance { get; set; }
+        public int ServiceSkjemaId { get; set; }
+        public List<ServiceSkjemafieldGroupModel> skjemaGroups { get; set; }
+
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerStreet { get; set; }
+        public string CustomerCity { get; set; }
+        public string CustomerZipcode { get; set; }
+        public string CustomerTelephoneNumber { get; set; }
+
+    }
+
+    public class ServiceSkjemafieldGroupModel
+    {
+        public string Name { get; set; }
+
+        public List<string> SkjemaFelt { get; set; }
+    }
+
+}
+
+
